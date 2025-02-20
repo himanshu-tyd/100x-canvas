f82beeb8df300cf02ecbfe0b97913b10a1552b2c
himanshu-tyd <himanshutaviyad0072@gmail.com>
2025-02-20 22:02:24 +0530

    fix: tailwind.config Uppercase

--- apps/drawcanvas/tailwind.config.ts
+++ apps/drawcanvas/tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./public/**/*.html",
     "./lib/**/*.{js,ts,jsx,tsx}",
