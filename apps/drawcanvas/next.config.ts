190ddf1e6d62cfae03b57bdedff858a755f227cb
himanshu-tyd <himanshutaviyad0072@gmail.com>
2025-02-22 22:18:48 +0530

    feat: change in verce.json

--- apps/drawcanvas/next.config.ts
+++ apps/drawcanvas/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "standalone", // Ensures proper bundling
-  experimental: {
-  },
   images: {
     domains: ["images.unsplash.com"],
   },
